51576eb46d946c306208f3002020a8f04a7a6689
Kristina Romanova <hrusha@freenet.de>
2023-07-20 00:46:21 +0200

    init

---
+++ script.js
@@ -0,0 +1,22 @@
+/* Необходимо получить массив объектов ToDO 
+с сервера https://jsonplaceholder.typicode.com/users/1/todos
+ используя fetch (см. код написанный в классе) и отобразить их на странице*/
+
+const objElement = document.getElementById("text");
+
+async function loadArray(){
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/1/todos');
+    const obj = await res.json();
+    console.log(obj);
+    obj.forEach(element => {
+        console.log(element);
+        const {userId, id, title, completed}= element;
+        objElement.textContent += `userId: ${userId} id: ${id} title: ${title} completed: ${completed}`;
+    });
+} 
+ loadArray();
+
+ 
+
+
+
